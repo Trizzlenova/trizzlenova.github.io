7297c8f295e6eff662051d72edeaebf803864d49
Trizzlenova <tdswayzee@gmail.com>
2018-05-17 16:15:35 -0700

    added a few more ideas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(window).scroll(function(){
   $("header").css("opacity", 1 - $(window).scrollTop() / 560);
-  // $("#about").css("opacity", 1 - $(window)
 });
 
-$(window).scroll(function(){
-});
+$(document).on('scroll', function(){
+  var currPos = $(document).scrollTop();
+  var fader = $('.fader');
+
+  fader.each(function(){
+    var offset = $(this).offset().top;
+
+    if(currPos > offset) {
+      $(this).css('opacity', 1 - (currPos - offset) / 200)
+    }
+  })
+})
 
 // console.log("works!")
 
@@ -17,3 +26,12 @@ $(".fadeScroll").slick({
   prevArrow: false,
   nextArrow: false
 });
+
+
+
+
+Play a music note for each picture
+that you scroll over. Need to create
+an object library for each image
+I also need to find a js music library
+
