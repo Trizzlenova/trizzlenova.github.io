70acd5b6161485c18adcde0084b3f960675e79c5
Trizzlenova <tdswayzee@gmail.com>
2018-05-17 14:34:52 -0700

    header/about carousel looking dope

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("works!")
+$(window).scroll(function(){
+  $("header").css("opacity", 1 - $(window).scrollTop() / 560);
+  // $("#about").css("opacity", 1 - $(window)
+});
+
+$(window).scroll(function(){
+});
+
+// console.log("works!")
+
+$(".fadeScroll").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 1000,
+  fade: true,
+  prevArrow: false,
+  nextArrow: false
+});
