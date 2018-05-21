614359d3e09d131f46ea5444249cef0a456e65e3
Trizzlenova <tdswayzee@gmail.com>
2018-05-20 17:16:46 -0700

    added space api

--- js/main.js
+++ js/main.js
@@ -2,31 +2,6 @@ $(window).scroll(function(){
   $("header").css("opacity", 1 - $(window).scrollTop() / 560);
 });
 
-$(document).on('scroll', function(){
-  var currPos = $(document).scrollTop();
-  var fader = $('.fader');
-
-  fader.each(function(){
-    var offset = $(this).offset().top;
-
-    if(currPos > offset) {
-      $(this).css('opacity', 1 - (currPos - offset) / 300);
-    }
-  })
-})
-
-// console.log("works!")
-
-// $(".fadeScroll").slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   speed: 1000,
-//   fade: true,
-//   arrows: true,
-//   dots: false,
-// });
-
 $('.fadeScroll').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
@@ -49,11 +24,31 @@ $('a').click(function(){
 // an object library for each image
 // I also need to find a js music library
 
-
-//
-
 // $('#projects img').on('mouseenter', function(){
 //   console.log("hover")
 //   var sound = new Audio('sounds/exampleNote.wav');
 //   sound.play();
 // });
+
+var url = "https://api.nasa.gov/planetary/apod?api_key=taN7yRSSTWnRu4IeLrwlfaZJuD70ldfEFMyIdXm7";
+
+
+$(document).ready(function() {
+  console.log("Let's get coding!");
+  $.ajax({
+    method: 'GET',
+    url: url,
+    success: function (response) {
+      console.log(response);
+      console.log(response.date);
+      console.log(response.url);
+      $('#space h1').append(response.title);
+      $('#space h3').append(response.date);
+      $('#space p').append(response.explanation);
+      $('#spacePic').attr('src', response.url);
+    }
+  });
+    // $('spacePic').attr('src', response.url);
+    // $('space h1').text(response.title);
+})
+
